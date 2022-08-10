8c3ace99b66bb3e8cde96e3ecde4052bca2241d7
Reshma R <37273172+reshukr97@users.noreply.github.com>
2022-08-11 03:46:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+//We will be using Browsers Local storage to store SCORE
+//Random function to have two random numbers.
+
+const scoreEl = document.getElementById("score");
+const questionEl = document.getElementById('question');
+const inputEl = document.getElementById("input-text");
+const btnEl = document.getElementById("btn");
+const formEl = document.getElementById("form");
+
+
+const num1 = Math.ceil(Math.random()*10);
+const num2 = Math.ceil(Math.random()*10);
+
+questionEl.innerText = ` ${num1} * ${num2} ?` ;
+const corrAns = num1 * num2;
+
+let score =  JSON.parse(localStorage.getItem("score_val"));
+
+if( score < 0)
+{
+    score = 0;
+}
+
+formEl.addEventListener("submit", ()=> {
+    let userAns = +inputEl.value;
+
+    if(userAns === corrAns)
+    {
+        score ++;
+        updateLocalStorage();
+    }
+    else
+    {
+        score --;
+        updateLocalStorage();
+    }
+});
+
+
+//// Key:String, Values: String
+function updateLocalStorage(){
+        localStorage.setItem("score_val", JSON.stringify(score)); 
+}
+
+scoreEl.innerText = `score: ${score}`;
+
+
+
